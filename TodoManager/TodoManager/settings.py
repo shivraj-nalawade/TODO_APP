e87ee1b445e11f2dc6d0058800572434b56d04b0
shivraj-nalawade <shivrajnalawade77@gmail.com>
2026-06-01 16:10:26 +0530

    small fix

--- TodoManager/TodoManager/settings.py
+++ TodoManager/TodoManager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!_+9&_)^ihx^)3@r0e&6hq^4^ob9=)1%*fuu^+^+cj7ytn_lv5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
